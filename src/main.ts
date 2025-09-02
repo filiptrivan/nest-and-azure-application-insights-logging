0b7057c1335e491fd740181c7ddd4a760544d8b3
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2025-09-02 18:39:33 +0200

    Project init

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { OpenTelemetryTransportV3 } from '@opentelemetry/winston-transport';
+import { useAzureMonitor } from '@azure/monitor-opentelemetry';
+
+useAzureMonitor({
+  azureMonitorExporterOptions: {
+    connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING
+  }
+});
+
+async function bootstrap() {
+  const winstonLogger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.ms(),
+          winston.format.simple(),
+        ),
+      }),
+      new OpenTelemetryTransportV3(),
+    ],
+  });
+
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      instance: winstonLogger,
+    }),
+  });
+
+  await app.listen(3000);
+}
+
+bootstrap();
